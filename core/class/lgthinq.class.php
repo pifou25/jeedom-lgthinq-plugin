617171fcb648ecf282a403410e8a4a759d9b78e9
pifou25 <nicolas_gras@hotmail.com>
2020-06-09 15:06:12 +0200

    remove pip dep checking

--- core/class/lgthinq.class.php
+++ core/class/lgthinq.class.php
@@ -220,16 +220,16 @@ class lgthinq extends eqLogic {
 					}
 				}
 
-				if ($return['state'] == 'ok'){
-					$cmd = "${daemonDir}check.sh";  // "source ${daemonDir}env/bin/activate && " . 'pip3 list | grep -Ec "wideq|Flask|requests"';
-					$deps = shell_exec($cmd);
-					if($deps < 5) {
-						LgLog::debug("missing pip dependancies ($deps) ($cmd)");
-						$return['state'] = 'nok';
-					} else {
-						$return['state'] = 'ok';
-					}
-				}
+				// if ($return['state'] == 'ok'){
+				// 	$cmd = "${daemonDir}check.sh";  // "source ${daemonDir}env/bin/activate && " . 'pip3 list | grep -Ec "wideq|Flask|requests"';
+				// 	$deps = shell_exec($cmd);
+				// 	if($deps < 5) {
+				// 		LgLog::debug("missing pip dependancies ($deps) ($cmd)");
+				// 		$return['state'] = 'nok';
+				// 	} else {
+				// 		$return['state'] = 'ok';
+				// 	}
+				// }
 			}
 			return $return;
 		}

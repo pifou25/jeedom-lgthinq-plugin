636e18bf09336842a0c34db2ffb2d393c30ed861
pifou <nicolas_gras@hotmail.com>
2020-12-20 10:40:52 +0100

    fix few log concat errors

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -36,7 +36,7 @@ function initLgthinqParameters() {
         }
     }
     $dir = realpath(lgthinq::getResourcesPath());
-    if(!is_dir($dir)){
+    if(!empty($dir) && !is_dir($dir)){
         if (!mkdir($dir, 0777, true))
             LgLog::error('unable to create dir ' . $dir);
         else

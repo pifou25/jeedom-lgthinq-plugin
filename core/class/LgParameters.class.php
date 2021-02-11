b21ef831095c7481d82f709243512e52c6babadd
Nicolas <nicolas_gras@hotmail.com>
2021-02-11 22:35:12 +0100

    Update LgParameters.class.php

--- core/class/LgParameters.class.php
+++ core/class/LgParameters.class.php
@@ -9,25 +9,26 @@
 class LgParameters {
 
     private static $log = '';
+
     /**
      * contains img, smallImg, lang, lg (for LG json config) and 
      * jeedom (for Jeedom json config)
      */
     const DATA_PATH = '/../../data';
     const RESOURCES_PATH = '/../../data/jeedom';
- 
+
     public static function getDataPath() {
         static $result = null;
-        if($result == null){
-            $result = realpath(__DIR__ . self::DATA_PATH).'/';
+        if ($result == null) {
+            $result = realpath(__DIR__ . self::DATA_PATH) . DIRECTORY_SEPARATOR;
         }
         return $result;
     }
 
     public static function getResourcesPath() {
         static $result = null;
-        if($result == null){
-            $result = realpath(__DIR__ . self::RESOURCES_PATH).'/';
+        if ($result == null) {
+            $result = realpath(__DIR__ . self::RESOURCES_PATH) . DIRECTORY_SEPARATOR;
         }
         return $result;
     }
@@ -37,19 +38,19 @@ class LgParameters {
      * @var array
      */
     private $devices = [];
-    
+
     /**
      * the LG auth URL
      * @var string
      */
     private $authUrl = null;
-    
+
     /**
      * LG account language-country
      * @var string 5 char (fr-FR)
      */
     public $language = null;
-    
+
     /**
      * LG account country
      * @var string 2 char (FR)
@@ -127,14 +128,14 @@ class LgParameters {
         }
         return $result;
     }
-    
+
     /**
      * build unique key for the json model
      * @param type $json
      * @return type
      */
-    private static function getDeviceKey($json){
-        return self::clean($json['Info']['modelType'].'-'.$json['Info']['productType']);
+    private static function getDeviceKey($json) {
+        return self::clean($json['Info']['modelType'] . '-' . $json['Info']['productType']);
     }
 
     /**
@@ -171,9 +172,9 @@ class LgParameters {
      * @param array $lg
      * @return array
      */
-    public static function convertLgToJeedom($lg){
+    public static function convertLgToJeedom($lg) {
         $config = self::getConfigInfos($lg);
-        $config['commands'] = array_merge( $config['commands'], self::getConfigActions($lg));
+        $config['commands'] = array_merge($config['commands'], self::getConfigActions($lg));
         return $config;
     }
 
@@ -211,7 +212,7 @@ class LgParameters {
         } else if (!self::isIndexArray($device['Config'], 'visibleItems')) {
             self::$log .= "\tno visibleItems for model_info () into config;\n";
         } else {
-            foreach($device['Config']['visibleItems'] as $key => $value){
+            foreach ($device['Config']['visibleItems'] as $key => $value) {
                 $name = $value['Feature'];
                 $cmt = self::getComment($protocol, $name);
                 self::$log .= "action $name: $cmt\n";
@@ -234,7 +235,7 @@ class LgParameters {
      * extract Monitoring-protocol from device config
      * @param type $device
      */
-    public static function getProtocol($device){
+    public static function getProtocol($device) {
         if (!self::isIndexArray($device, 'Monitoring')) {
             self::$log .= "\tno monitoring for model ();\n";
         } else if (!self::isIndexArray($device['Monitoring'], 'protocol')) {
@@ -258,7 +259,7 @@ class LgParameters {
 
     // return $arr[$result] where $arr[$key] = $value
     public static function getInArray($arr, $key, $value, $result) {
-        if (!empty($arr)){
+        if (!empty($arr)) {
             foreach ($arr as $arr0) {
                 if (isset($arr0[$key]) && $arr0[$key] == $value && isset($arr0[$result])) {
                     return $arr0[$result];
@@ -325,7 +326,9 @@ class LgParameters {
      * @return array of json file names
      */
     public static function getAllConfig() {
-        return array_diff(scandir(self::getResourcesPath()), ['.', '..']);
+        $dir = self::getResourcesPath();
+        LgLog::debug("Scan $dir");
+        return array_diff(scandir($dir), ['.', '..']);
     }
 
     public static function getLog() {
@@ -337,56 +340,43 @@ class LgParameters {
      * @param string $url : https://api.github.com/repos/[user]/[repo]/branches
      * @return array
      */
-    public static function getGithubBranches($url){
+    public static function getGithubBranches($url) {
         $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 4);
-        curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 
         $json = curl_exec($ch);
         $branches = [];
-        if(!$json) {
+        if (!$json) {
             $branches['error'] = curl_error($ch);
-        } else if(empty($json)){
+        } else if (empty($json)) {
             $branches['error'] = 'empty response';
-        }else{
+        } else {
             $json = json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
-              foreach($json as $data){
-                      $branches[] = $data['name'];
+            foreach ($json as $data) {
+                $branches[] = $data['name'];
             }
         }
         curl_close($ch);
         return $branches;
     }
 
-    /**
-     * search file name into $url with the $regex, then copy $url at $dest with $name
-     * @param string $url source to copy
-     * @param string $regex to capture the name
-     * @param string $dest destination top copy file
-     * @return true if success; otherwise: error message
-     */
-    public static function copyDataRegex($url, $regex, $dest) {
-        $found = preg_match($regex, $url, $matches);
-        if ($found) {
-            return self::copyDataFromUrl($url, $matches[1], $dest);
-        }
-        return "Copy error: no matche $regex";
-    }
-
-    public static function downloadAndCopyDataModel($id, $_model){
+    public static function downloadAndCopyDataModel($id, $_model) {
         // download images and json config from LG cloud
-        $msg[] = self::copyDataFromUrl($_model['smallImageUrl'], $id.'.png', self::getDataPath(). 'smallImg/');
-        $msg[] = self::copyDataFromUrl($_model['modelJsonUrl'], $id.'.json', self::getDataPath().'lg/');
-        $msg[] = self::copyDataFromUrl($_model['langPackProductTypeUri'], $id.'.json', self::getDataPath().'lang/');
+        $msg[] = self::copyDataFromUrl($_model['smallImageUrl'], $id . '.png', self::getDataPath() . 'smallImg/');
+        $msg[] = self::copyDataFromUrl($_model['modelJsonUrl'], $id . '.json', self::getDataPath() . 'lg/');
+        $msg[] = self::copyDataFromUrl($_model['langPackProductTypeUri'], $id . '.json', self::getDataPath() . 'lang/');
 
         // transform LG json config into Jeedom json
-        $file = self::getDataPath().'lg/'.$id . '.json';
-        $lg = json_decode( file_get_contents($file), true, 512, JSON_BIGINT_AS_STRING);
+        $file = self::getDataPath() . 'lg/' . $id . '.json';
+        $lg = json_decode(file_get_contents($file), true, 512, JSON_BIGINT_AS_STRING);
         $data = json_encode(self::convertLgToJeedom($lg), JSON_PRETTY_PRINT);
         $msg[] = self::copyData($data, "$id.json", self::getResourcesPath());
-        LgLog::debug("copy img and json datas. " . print_r(array_filter($msg, function($v){return $v!==true;}), true));
+        LgLog::debug("copy img and json datas. " . print_r(array_filter($msg, function($v) {
+                            return $v !== true;
+                        }), true));
     }
 
     /**
@@ -398,17 +388,18 @@ class LgParameters {
      * @return boolean or error message
      */
     public static function copyData($data, $name, $dest) {
-        if(file_exists($dest . $name)){
+        if (file_exists($dest . $name)) {
             unlink($dest . $name);
         }
-        if (!is_dir($dest) && !mkdir($dest, 0777, true)){
+        if (!is_dir($dest) && !mkdir($dest, 0777, true)) {
             return "unable to create dir $dest";
         }
-        if (file_put_contents($dest . $name, $data) === false){
+        if (file_put_contents($dest . $name, $data) === false) {
             return "Erreur lors de l'écriture vers $dest$name";
         }
         return true;
     }
+
     /**
      * copy $url file into $dest/$name. create $dest directory if it doesn't exists.
      * @param type $url source to copy
@@ -416,9 +407,9 @@ class LgParameters {
      * @param type $dest : directory destination
      * @return boolean or error message
      */
-    public static function copyDataFromUrl($url, $name, $dest){
+    public static function copyDataFromUrl($url, $name, $dest) {
         $data = file_get_contents($url);
-        if($data === false){
+        if ($data === false) {
             return "Erreur lors de la lecture du fichier $url";
         }
         return self::copyData($data, $name, $dest);
@@ -428,16 +419,16 @@ class LgParameters {
      * scan and zip every files, and download it.
      * @param array $dirs = ['lg/', 'jeedom/', 'lang/']
      */
-    public static function zipConfig($dirs, $tmp_file = '/tmp/lgthinq.zip'){
-        if(create_zip($dirs, $tmp_file)){
+    public static function zipConfig($dirs, $tmp_file = '/tmp/lgthinq.zip') {
+        if (create_zip($dirs, $tmp_file)) {
             self::download($tmp_file);
             return "Archive created! $nb files, $i added, $err errors to $filename ($status)";
-        }else{
+        } else {
             return "error preparing zip $tmp_file";
         }
     }
-    
-    public static function download($file){
+
+    public static function download($file) {
 
         // http headers for zip downloads
         header("Pragma: public");
@@ -449,8 +440,8 @@ class LgParameters {
         // header('Content-type: application/zip');
         header("Content-Disposition: attachment; filename=\"lgthinq.zip\"");
         header("Content-Transfer-Encoding: binary");
-        header("Content-Length: ".filesize($file));
+        header("Content-Length: " . filesize($file));
         readfile($file);
-        
     }
+
 }
